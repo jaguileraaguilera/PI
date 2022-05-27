1329459102b7089539dd72fcd951ad98c8424611
Javier Aguilera Aguilera <javieraguilerayaguilera@gmail.com>
2022-05-27 11:41:24 +0200

    Corregida vista entrega/alta.php Mostraba id de plantaciones que son actuales

--- views/input/id_plantacion_select.php
+++ views/input/id_plantacion_select.php
@@ -1,9 +1,11 @@
 <select class="form-select" id="<?=$atributo?>" name="<?=$atributo?>" required>
     <?php foreach ($plantaciones as $plantacion): ?>
-        <?php if ($plantacion -> getIdPlantacion() == $valor): ?>
-            <option selected value="<?= $valor?>"><?= $valor?></option>
-        <?php else: ?> 
-            <option value="<?=$plantacion -> getIdPlantacion()?>"><?=$plantacion -> getIdPlantacion()?></option>
+        <?php if ($plantacion -> getActual() == 1): ?>
+            <?php if ($plantacion -> getIdPlantacion() == $valor): ?>
+                <option selected value="<?= $valor?>"><?= $valor?></option>
+            <?php else: ?> 
+                <option value="<?=$plantacion -> getIdPlantacion()?>"><?=$plantacion -> getIdPlantacion()?></option>
+            <?php endif; ?>
         <?php endif; ?>
     <?php endforeach; ?> 
 </select>
