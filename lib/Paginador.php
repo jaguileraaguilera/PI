1ae7134e05ce463a78822eacafae08141322dfff
Javier Aguilera Aguilera <javieraguilerayaguilera@gmail.com>
2022-06-06 14:56:02 +0200

    Documentado Paginador.php

--- lib/Paginador.php
+++ lib/Paginador.php
@@ -1,12 +1,22 @@
 <?php
 namespace lib;
 
+/**
+ * Paginador
+ */
 class Paginador {
     public array $datos;
     public array $particiones; 
     public int $items_vista;
     public int $num_paginas;
-
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $datos
+     * @param  mixed $items_vista
+     * @return void
+     */
     function __construct($datos, $items_vista) {
         $this -> items_vista = $items_vista;
         $this -> particiones = array_chunk($datos, $items_vista);
